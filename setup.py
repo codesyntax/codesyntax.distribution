ef29b3930ef3e39678905082b76688fdb2a35d03
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-01-05 14:41:33 +0100

    remove testing

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
     install_requires=[
         "Plone",
         "setuptools",
+        "plone.api",
         "plone.distribution",
         "codesyntax.login",
         "collective.easyform",
@@ -68,12 +69,6 @@ setup(
     extras_require={
         "test": [
             "plone.app.testing",
-            # Plone KGS does not use this version, because it would break
-            # Remove if your package shall be part of coredev.
-            # plone_coredev tests as of 2016-04-01.
-            "plone.testing>=5.0.0",
-            "plone.app.contenttypes",
-            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
